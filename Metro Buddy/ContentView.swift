6303ae2c7eb96749d34d82f25c56c1eaa154a4b0
Alexis Aubry <alexisonline@fastmail.com>
2020-07-06 18:55:05 -0400

    Initial Commit

---
+++ Metro Buddy/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Metro Buddy
+//
+//  Created by Alexis Aubry on 7/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metro Buddy/Metro_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metro_BuddyApp.swift
+//  Metro Buddy
+//
+//  Created by Alexis Aubry on 7/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Metro_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
